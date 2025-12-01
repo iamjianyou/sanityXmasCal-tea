ee66370f0b30174abb2e8b872a8ff678cb9a99d7
Jianyou Dai <jianyou.dai@elmeragroup.no>
2025-12-02 00:39:25 +0100

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center gap-2">
       <h2 className="text-3xl font-bold">Hei! Jeg heter Jian You</h2>
-      <p className="text-2xl"> og jeg liker å drikke te</p>
+      <p className="text-2xl"> og jeg liker å drikke te 茶</p>
       <Image
         src="/kungfuTea.png"
         alt="Description"
